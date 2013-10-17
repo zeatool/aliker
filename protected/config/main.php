92c5da7632151c0ca19bdc86f796d5e726284a8f
ZeatooL <i@romqa.ru>
2013-10-17 22:42:26 +0800

    ----

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Panda tracker',
+	'name'=>'Aliker',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
