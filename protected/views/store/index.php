77a5e38ddea6d16daf309a7fbd7c41c7138b10ff
Roman Baranov <i@romqa.ru>
2013-10-25 09:49:02 +0800

    ----

---
+++ protected/views/store/index.php
@@ -0,0 +1,17 @@
+<?php
+$this->breadcrumbs=array(
+	'Stores',
+);
+
+$this->menu=array(
+	array('label'=>'Create Store','url'=>array('create')),
+	array('label'=>'Manage Store','url'=>array('admin')),
+);
+?>
+
+<h1>Stores</h1>
+
+<?php $this->widget('bootstrap.widgets.TbListView',array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+)); ?>
